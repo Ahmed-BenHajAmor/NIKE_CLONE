484ee0cde23a1bee38053e23588a9df5443fbdeb
Ahmed-BenHajAmor <142888862+Ahmed-BenHajAmor@users.noreply.github.com>
2023-10-29 10:55:44 +0100

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,8 +2,8 @@ import React, { useContext } from 'react'
 import './navbar.css'
 import { Link } from 'react-router-dom'
 import { Context } from '../../App'
-import  logo  from '/home/ahmed/ReactProject/NikeClone/Nike-clone/src/assets/logo.png';
-import  bag  from '/home/ahmed/ReactProject/NikeClone/Nike-clone/src/assets/bag.png';
+import  logo  from '/src/assets/logo.png';
+import  bag  from '/src/assets/bag.png';
 
 function Navbar() {
   const {dispatch} = useContext(Context)
