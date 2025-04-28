5dda80831a62b858635de07e7f5e269216cb7df3
Ahmed <ahmed.benhajamorr@gmail.com>
2025-04-28 20:15:52 +0100

    api fix

--- src/components/sales/Sales.jsx
+++ src/components/sales/Sales.jsx
@@ -1,4 +1,5 @@
-import React, { useContext, useEffect } from 'react'
+/* eslint-disable react/prop-types */
+import React, { useContext } from 'react'
 import './sales.css'
 import { Context } from '../../App'
 import { Card } from '../card/Card'
@@ -18,14 +19,16 @@ function Sales() {
 }
 
 const SubSale = ({title, data})=>{
+  console.log(data);
   return(
     <div className='sub-sale'>
       <div className="title"><h3>{title}</h3></div>
       <div className="products">
         {
           
+          
           data?.map((product, key)=>{
-            return product.title === 'Email a Gift Card' ? '' : <Card key={key} img={product?.spriteSheet} title={product.title} price={product.rawPrice}/>
+            return product.title === 'Email a Gift Card' ? '' : <Card key={key} img={product?.detailPictureFive} title={product.title} price={product.rawPrice}/>
           })
         }
         
